b4d47ad6d0a80921554fc526479d15ee78abe0ab
hioncrypto <hionforex@gmail.com>
2025-09-19 10:39:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 # pandas>=2.0
 # numpy>=1.24
 # requests>=2.31
-# websocket-client>=1.6
+# websocket-client>=1.6 
 
 import json
 import time
@@ -22,7 +22,7 @@ import numpy as np
 import pandas as pd
 import requests
 import streamlit as st
-
+from streamlit_autorefresh import st_autorefresh
 # ----------------------------- App setup -----------------------------
 st.set_page_config(page_title="Crypto Tracker by hioncrypto", layout="wide")
 
