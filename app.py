a12b4c204e4ae0d9f4ef8a25ae5893b84feb5823
hioncrypto <hionforex@gmail.com>
2025-10-08 13:01:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -243,7 +243,7 @@ def init_session_state():
     }
     
     # CRITICAL: Only set defaults for keys that DON'T exist
-for key, value in defaults.items():
+        for key, value in defaults.items():
         if key not in st.session_state:
             st.session_state[key] = value
 
