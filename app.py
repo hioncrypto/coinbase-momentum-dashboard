191982d70114cac30a10cb3d047c76e87573681b
hioncrypto <hionforex@gmail.com>
2025-12-13 23:43:10 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1869,7 +1869,6 @@ if rows:
     chg_col = f"% Change ({sort_tf})"
     ascending = not st.session_state["sort_desc"]
     df_results = df_results.sort_values(chg_col, ascending=ascending)
-    )
     df_results.insert(0, "#", range(1, len(df_results) + 1))
 
     green_count = df_results["_green"].sum()
