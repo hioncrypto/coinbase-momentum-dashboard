462740ff686fefe7a533bb94db24cdde51d2d84d
hioncrypto <hionforex@gmail.com>
2026-03-16 08:04:02 -0700

    Change gate_mode from 'ANY' to 'ALL'

--- app.py
+++ app.py
@@ -358,7 +358,7 @@ def init_session_state():
         "macd_cross_only_bull": True,
         "macd_cross_below_zero": True,
         "macd_hist_confirm_bars": 3,
-        "gate_mode": "ANY",
+        "gate_mode": "ALL",
         "hard_filter": False,
         "K_green": 3,
         "Y_yellow": 2,
