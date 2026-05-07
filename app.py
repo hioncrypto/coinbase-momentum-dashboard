08fc933636354818fb267693e0cfbb8f81c61da1
hioncrypto <hionforex@gmail.com>
2026-05-07 07:57:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1173,7 +1173,7 @@ with st.sidebar:
     st.title("🚀 Crypto Tracker")
 
     c1, c2, c3 = st.columns([1, 1, 1])
-with c1:
+    
 def collapse_all():
     st.session_state["collapse_all"] = True
 
@@ -1181,7 +1181,6 @@ def expand_all():
     st.session_state["collapse_all"] = False
 
 with c1:
-    if
     if st.button("Collapse All", key="collapse_btn", on_click=collapse_all):
         st.rerun()
 
